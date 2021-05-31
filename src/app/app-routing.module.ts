bd1b5d32e1997d020252e1ff969e44498c90e04a
dominici.guillaume@gmail.com <dominici.guillaume@gmail.com>
2021-05-31 14:36:35 +0200

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { AppareilComponent } from './components/appareil/appareil.component';
-// import { AppareilViewComponent } from './components/appareil-view/appareil-view.component';
+import { AppareilViewComponent } from './components/appareil-view/appareil-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'blog', component: BlogComponent },
-  { path: 'appareil', component: AppareilComponent },
+  { path: 'appareil', component: AppareilViewComponent },
 ];
 
 @NgModule({
